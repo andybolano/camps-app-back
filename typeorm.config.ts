47d18276e3148ee43be2bd1110673dc2b54089a3
andyb <andyb@solvento.mx>
2025-04-11 08:24:33 -0500

    fix typeorm

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -4,10 +4,28 @@ import { join } from 'path';
 
 config();
 
-export default new DataSource({
-  type: 'sqlite',
-  database: join(__dirname, 'db.sqlite'),
+const isProduction = process.env.NODE_ENV === 'production';
+
+const baseConfig = {
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
-  synchronize: process.env.NODE_ENV !== 'production',
-});
+  synchronize: !isProduction,
+};
+
+const configOptions = isProduction
+  ? {
+      ...baseConfig,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl:
+        process.env.DATABASE_SSL === 'true'
+          ? { rejectUnauthorized: false }
+          : false,
+    }
+  : {
+      ...baseConfig,
+      type: 'sqlite',
+      database: join(__dirname, 'db.sqlite'),
+    };
+
+export default new DataSource(configOptions as any);
